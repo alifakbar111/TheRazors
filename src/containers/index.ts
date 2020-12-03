0103d2d43cb6796c101a67174520649a18e21906
alifakbar111 <alifakbar111@gmail.com>
2020-12-04 00:09:17 +0700

    feat: configure login page

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,5 +1,6 @@
 import Products from "./Products/index.svelte";
 import FeaturedProd from "./FeaturedProd/index.svelte";
 import Cart from "./Cart/index.svelte";
+import Login from "./Login/index.svelte";
 
-export { Products, FeaturedProd, Cart };
+export { Products, FeaturedProd, Cart, Login };
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,19 @@
 import { writable } from "svelte/store";
 
-const user = writable({ username: null, jwt: null });
-
-export default user;
+const userStorage = writable(getStorageUser());
+function getStorageUser() {
+  return localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : { username: null, jwt: null };
+}
+export function setStorageUser(user: { username: string; jwt: string; }) {
+  localStorage.setItem("user", JSON.stringify(user));
+}
+export function setUser(user: { username: string; jwt: string; }) {
+  userStorage.set(user);
+}
+export function logoutUser() {
+  localStorage.clear();
+  userStorage.set({ username: null, jwt: null });
+}
+export default userStorage;
